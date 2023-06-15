63fd0a1e30385da47308d7f542bbd4a4d341f3cc
Chris Wood <wchristopherwood912@gmail.com>
2023-06-15 11:49:07 -0400

    file uploads now work

--- controllers/docs.js
+++ controllers/docs.js
@@ -1,6 +1,16 @@
+/* eslint-disable new-cap */
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 module.exports = (app) => {
+  // Upload
   app.get('/', (req, res) => {
-    res.render('index.handlebars');
+    res.render('upload.handlebars');
+  });
+  app.post('/docs/upload', upload.single('doc'), (req, res) => {
+    console.log(req.file);
+    if (req.file) {
+    }
+    res.redirect('/');
   });
 };
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 /* eslint-disable new-cap */
+const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // controllers
 require('./controllers/docs')(app);
