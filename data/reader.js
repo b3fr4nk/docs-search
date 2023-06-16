ef335730940c28185e033fffef31c07766d05dad
Chris Wood <wchristopherwood912@gmail.com>
2023-06-16 13:14:04 -0400

    renamed to better represent intended function

--- data/reader.js
+++ data/reader.js
@@ -3,10 +3,9 @@ const fs = require('fs');
 module.exports = (doc) => {
   fs.readFile(doc, 'utf8', (err, data) => {
     if (err) {
-      console.error(err);
+      console.log(err);
       return;
     }
-    console.log(data);
     return data;
   });
 };
