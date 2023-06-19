d796b57af87ef19911e6057483263026d7812535
Chris Wood <wchristopherwood912@gmail.com>
2023-06-19 09:48:21 -0400

    now splits the data into sentences

--- data/reader.js
+++ data/reader.js
@@ -1,8 +1,31 @@
 const fs = require('fs');
 
 module.exports = (doc) => {
+  const sentences = 3;
+  const overlap = 1;
   try {
-    const data = fs.readFileSync(doc, 'utf8');
+    let str = fs.readFileSync(doc, 'utf8');
+    str = str.replace(/(\r\n|\n|\r)/gm, ' ');
+    str = str.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
+
+    // splits string into number of sentences with number of overlap sentences
+    const data = [];
+    for (let i = sentences; i < str.length; i+=sentences) {
+      let entry = '';
+      // only does an overlap after the first set of sentences
+      if (i === sentences) {
+        for (let j = sentences; j > 0; j--) {
+          entry = entry + str[i - j];
+        }
+      } else {
+        for (let j = sentences + overlap; j > 0; j--) {
+          entry = entry + str[i - j];
+        }
+      }
+
+      data.push(entry);
+    }
+
     return data;
   } catch (err) {
     console.log(err);
